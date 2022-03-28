f4ec3b78a98fc9405c46db9e76e58322ce361f10
Bill Cathey <billcathey@hotmail.com>
2022-03-28 16:25:10 -0400

    Set state to data

--- src/Row.js
+++ src/Row.js
@@ -1,16 +1,18 @@
 import React, {useState, useEffect} from 'react'
 import axios from './axios'
 
-function Row ({title, fetchUrl}) {
+function Row({title, fetchUrl}) {
     const [movies, setMovies] = useState([])
     useEffect(() => {
         async function fetchData() {
             const request = await axios.get(fetchUrl)
-            console.log(request)
+            setMovies(request.data.results)
             return request
         }
         fetchData()
-    }, [])
+    }, [fetchUrl])
+    console.log(movies)
+
     return (
         <div>
             <h2>{title}</h2>
