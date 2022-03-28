dd339057c2af54f51bc6b210bfe894ed3fb063ea
Bill Cathey <billcathey@hotmail.com>
2022-03-28 16:10:35 -0400

    Create Row component, fetch movies

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import requests from './requests'
+import Row from './Row';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
+      <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
