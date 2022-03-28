b68984ca6083c3168897052d3bdfcd61149a4f5d
Bill Cathey <billcathey@hotmail.com>
2022-03-28 16:38:47 -0400

    Fetch images, implement scroll behavior in rows

--- src/Row.js
+++ src/Row.js
@@ -1,5 +1,8 @@
 import React, {useState, useEffect} from 'react'
 import axios from './axios'
+import './Row.css'
+
+const base_url = "https://image.tmdb.org/t/p/original/"
 
 function Row({title, fetchUrl}) {
     const [movies, setMovies] = useState([])
@@ -14,8 +17,18 @@ function Row({title, fetchUrl}) {
     console.log(movies)
 
     return (
-        <div>
+        <div className='row'>
             <h2>{title}</h2>
+            <div className='row__posters'>
+                {movies.map(movie => (
+                    <img
+                        className='row__poster' 
+                        key={movie.id} 
+                        src={`${base_url}${movie.poster_path}`} 
+                        alt={movie.name} 
+                    />
+                ))}
+            </div>
         </div>
     )
 }
