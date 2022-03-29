5842cb72bfecba30f8ce48ec0ed79b0d7456549d
Bill Cathey <billcathey@hotmail.com>
2022-03-28 21:41:54 -0400

    Add fade effect to bottom of banner

--- src/Banner.js
+++ src/Banner.js
@@ -41,7 +41,8 @@ function Banner() {
                 <h1 className='banner__description'>
                     {truncate(movie?.overview, 150)}    
                 </h1>
-            </div>    
+            </div>
+            <div className='banner--fadeBottom' />
         </header>
     )
 }
