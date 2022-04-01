7aa8847ecb4e54fc4ec687107a99ced3d83e4b41
Bill Cathey <billcathey@hotmail.com>
2022-03-31 21:29:52 -0400

    Add SF movies, delete Netflix Originals

--- src/App.js
+++ src/App.js
@@ -9,13 +9,13 @@ function App() {
     <div className="App">
       <Nav />
       <Banner />
-      <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} isLargeRow />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
-      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
       <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
       <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
       <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
       <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Science Fiction Movies" fetchUrl={requests.fetchSFMovies} />
       <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
